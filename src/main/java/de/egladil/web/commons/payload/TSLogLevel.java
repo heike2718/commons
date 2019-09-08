16b87b41a0e436c8f2322aed55475aa45f764dca
Heike Winkelvoß <public@egladil.de>
2019-09-08 15:47:17 +0200

    client logs auf Server schicken

---
+++ src/main/java/de/egladil/web/commons/payload/TSLogLevel.java
@@ -0,0 +1,20 @@
+//=====================================================
+// Project: checklistenserver
+// (c) Heike Winkelvoß
+//=====================================================
+package de.egladil.web.commons.payload;
+
+/**
+ * TSLogLevel
+ */
+public enum TSLogLevel {
+
+	All,
+	Debug,
+	Info,
+	Warn,
+	Error,
+	Fatal,
+	Off;
+
+}
